25855f55cc9a929818f2ad9f8887bcc81b2e8956
GitEngHar <harukii1999@gmail.com>
2024-04-14 23:55:40 +0900

    update

---
+++ JavaNative/Books/honkaku/Dates/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        DataCalenderExit dce = new DataCalenderExit();
+        dce.ex6();
+    }
+}
---
+++ JavaNative/Books/honkaku/Dates/src/DataCalenderExit.java
@@ -0,0 +1,36 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class DataCalenderExit {
+    public void ex1(){
+        Date now = new Date();
+        System.out.println(now);
+    }   
+    public void ex2(){
+        Date old = new Date(5000);
+        System.out.println(old);
+    }
+    public void ex3(){
+       Calendar cl = Calendar.getInstance();
+       System.out.println(cl.getTime());  
+    }
+
+    public void ex4(){
+        Calendar cl = Calendar.getInstance();
+        Date dt = cl.getTime();
+        System.out.println(dt);  
+    }
+
+    public void ex5(){
+        Calendar cl = Calendar.getInstance();
+        Date dt = new Date(cl.getTimeInMillis());
+        System.out.println(dt);  
+    }
+
+    public void ex6(){
+        Date dt = new Date();
+        Calendar cl = Calendar.getInstance();
+        cl.setTime(dt);
+        System.out.println(cl.getTime());
+    }
+}
