49be29b3a273191fb4b46085d3d048fb722b26b2
GitEngHar <harukii1999@gmail.com>
2024-09-12 23:47:08 +0900

    Beantest

--- WorkUpServer/src/main/java/com/example/demo/web/TestController.java
+++ WorkUpServer/src/main/java/com/example/demo/web/TestController.java
@@ -1,15 +1,20 @@
 package com.example.demo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.service.HelloBean;
+
 @RestController
 @RequestMapping("/")
 public class TestController {
+	@Autowired
+	HelloBean helloBean;
 	@GetMapping("wp")
 	public String rootHandler() {
-		System.out.println("OK");
+		helloBean.hello();
 		return "OK";
 	}
 }
