cf3f733e435cff95aee4d85830789961a63e4d8b
GitEngHar <harukii1999@gmail.com>
2024-09-12 22:30:48 +0900

    projectstart

--- WorkUp-1/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO jquery_validate (name) 
-VALUES ('testuser');
-INSERT INTO jquery_validate (name) 
-VALUES ('testuser2');
-INSERT INTO jquery_validate (name) 
-VALUES ('testuse3');
-INSERT INTO contents_status_root (status) 
-VALUES ('active');
