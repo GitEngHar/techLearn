5dfd623156efedd699c258be9971f4e5981f74c4
GitEngHar <harukii1999@gmail.com>
2024-09-19 08:05:46 +0900

    @value

--- WorkUpServer/src/main/java/com/example/demo/service/HelloBean.java
+++ WorkUpServer/src/main/java/com/example/demo/service/HelloBean.java
@@ -1,9 +1,18 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloBean {
+	@Value("${beanTestValue.value}")
+	private String beanTestValue;
+	
+	public String confirmBeanTestValue() {
+		System.out.println("DiValue=" + beanTestValue);
+		return beanTestValue;
+	}
+	
 	public void hello() {
 		System.out.println("helloBean");
 	}
