ae545abf27a69c561258c80dba1aa4c26ceb121c
GitEngHar <harukii1999@gmail.com>
2024-09-20 07:08:18 +0900

    InMemoryUserDetailsManagerでゆーざーを追加

--- WorkUpServer/src/main/java/com/example/demo/conf/SecurityConfig.java
+++ WorkUpServer/src/main/java/com/example/demo/conf/SecurityConfig.java
@@ -3,11 +3,41 @@ package com.example.demo.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 // 設定のアノテーション 起動時の1回だけJavaコンテナに登録される
 @Configuration
 public class SecurityConfig {
+	@Bean
+	 PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	InMemoryUserDetailsManager userDetailsService() {
+		
+		UserDetails admin = User
+				.withUsername("admin")
+				.password(passwordEncoder().encode("admin"))
+				.roles("ADMIN")
+				.build();
+		UserDetails hoge = User
+				.withUsername("hoge2")
+				.password(passwordEncoder().encode("hoge2"))
+				.roles("USER")
+				.build();
+		
+		System.out.printf("admin -> [%s]\n", passwordEncoder().encode("admin"));
+		System.out.printf("hoge2 -> [%s]\n", passwordEncoder().encode("hoge2"));
+		
+		return new InMemoryUserDetailsManager(admin,hoge);
+	}
+	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.formLogin(login -> login // フォーム認証を使う
@@ -19,4 +49,5 @@ public class SecurityConfig {
 						);
 		return http.build();
 	}
+	
 }
